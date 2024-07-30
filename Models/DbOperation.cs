baa6a93b329dcbd34c1390244be2be1fdc8c0fb0
Karan Kumar  Sahu <102219640+Karan09823@users.noreply.github.com>
2024-07-30 23:17:57 +0530

    Add files via upload

--- Models/DbOperation.cs
+++ Models/DbOperation.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace REGISTRATION.Models
 {
@@ -23,9 +24,9 @@ namespace REGISTRATION.Models
         {
             List<User> list = new List<User>();
 
-            using (SqlCommand cmd = new SqlCommand("select * from registered_user", Con))
+            using (SqlCommand cmd = new SqlCommand("exec GETALLUSER", Con))
             {
-                
+
                 SqlDataAdapter adapter = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable();
                 adapter.Fill(dt);
@@ -37,7 +38,9 @@ namespace REGISTRATION.Models
                         registered_user_id = Convert.ToInt32(dr[0]),
                         registered_user_name = Convert.ToString(dr[1]),
                         registered_user_mail = Convert.ToString(dr[2]),
-                        registered_user_password = Convert.ToString(dr[3])
+                        registered_user_password = Convert.ToString(dr[3]),
+                        registered_user_about = Convert.ToString(dr[4]),
+                        registered_user_imagepath = Convert.ToString(dr[5])
 
                     });
 
@@ -48,16 +51,18 @@ namespace REGISTRATION.Models
             return list;
         }
 
-        public bool RegisteringUser(string name,string email,string password,string about)
+        public bool RegisteringUser(string name, string email, string password, string about, string imagepath)
         {
-            string query = "INSERT INTO registered_user (registered_user_name, registered_user_mail, registered_user_password, registered_user_about) VALUES (@Name, @Email, @Password, @About)";
+            string query = "INSERT INTO registered_user (registered_user_name, registered_user_mail, registered_user_password, registered_user_about, registered_user_imagepath) " +
+                           "VALUES (@Name, @Email, @Password, @About, @Image)";
+
             using (SqlCommand cmd = new SqlCommand(query, Con))
             {
                 cmd.Parameters.AddWithValue("@Name", name);
                 cmd.Parameters.AddWithValue("@Email", email);
                 cmd.Parameters.AddWithValue("@Password", password);
                 cmd.Parameters.AddWithValue("@About", about);
-
+                cmd.Parameters.AddWithValue("@Image", imagepath);
 
                 try
                 {
@@ -65,8 +70,10 @@ namespace REGISTRATION.Models
                     int result = cmd.ExecuteNonQuery();
                     return result > 0;
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
+                    // Log the exception message for debugging purposes
+                    Console.WriteLine(ex.Message);
                     return false;
                 }
                 finally
@@ -76,10 +83,11 @@ namespace REGISTRATION.Models
             }
         }
 
-        public User GetUserById(string userId)
+
+        public User GetUserById(int userId)
         {
             User user = null;
-            string query = "SELECT * FROM registered_user WHERE registered_user_id = @UserId";
+            string query = "exec USERBYID @Userid=@UserId";
 
             using (SqlCommand cmd = new SqlCommand(query, Con))
             {
@@ -97,16 +105,87 @@ namespace REGISTRATION.Models
                         registered_user_id = Convert.ToInt32(dr[0]),
                         registered_user_name = Convert.ToString(dr[1]),
                         registered_user_mail = Convert.ToString(dr[2]),
-                        registered_user_password = Convert.ToString(dr[3])
+                        registered_user_password = Convert.ToString(dr[3]),
+                        registered_user_about = Convert.ToString(dr[4]),
+                        registered_user_imagepath = Convert.ToString(dr[5])
+
                     };
+
+                   
                 }
             }
 
             return user;
+
+        }
+
+        /*--Edit--*/
+        public bool EditOperation(User editDetails) 
+        {
+            string query = "EXEC EDITUSER @EditUserId=@EditId," +
+                "@EditUserName=@EditName," +
+                "@EditUserAbout=@EditAbout," +
+                "@EditUserPassword=@EditPassword," +
+                "@EditUserMail=@EditMail,"+
+                "@EditUserImage=@EditImage";
+
+            using (SqlCommand cmd = new SqlCommand(query, Con))
+            {
+                cmd.Parameters.AddWithValue("@EditName", editDetails.registered_user_name);
+                cmd.Parameters.AddWithValue("@EditMail", editDetails.registered_user_mail);
+                cmd.Parameters.AddWithValue("@EditPassword", editDetails.registered_user_password);
+                cmd.Parameters.AddWithValue("@EditAbout",editDetails.registered_user_about);
+                cmd.Parameters.AddWithValue("@EditId", editDetails.registered_user_id);
+
+                cmd.Parameters.AddWithValue("@EditImage",editDetails.registered_user_imagepath);
+
+                try
+                {
+                    Con.Open();
+                    int result = cmd.ExecuteNonQuery();
+                    return result > 0;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                finally
+                {
+                    Con.Close();
+                }
+
+
+            }
+        }
+
+        /*Delete*/
+        public bool DeleteOperation(int deleteId)
+        {
+            string query = "EXEC DELETEUSER @DeleteUserId=@DelId";
+            using (SqlCommand cmd = new SqlCommand(query, Con))
+            {
+
+                cmd.Parameters.AddWithValue("@DelId", deleteId);
+
+                try
+                {
+                    Con.Open();
+                    int result = cmd.ExecuteNonQuery();
+                    return result > 0;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                finally
+                {
+                    Con.Close();
+                }
+
+            }
+
         }
 
 
     }
-
-
 }
